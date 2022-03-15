9c2e5e9e4bd5c28929c18a042a715fa2ddd1f5b5
Antonio Caggiano <info@antoniocaggiano.eu>
2022-03-15 17:56:22 +0100

    math: RGBA8
    Use a structure to represent a pixel color for the default png encoder.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,11 +2,13 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+pub mod color;
 pub mod ops;
 pub mod point3;
 pub mod ray;
 pub mod vec3;
 
+pub use color::*;
 pub use ops::*;
 pub use point3::*;
 pub use ray::*;
