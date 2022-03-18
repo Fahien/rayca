1ae53b63fcaafe7e951d31ab5fdbfb24b3c07f2b
Antonio Caggiano <info@antoniocaggiano.eu>
2022-03-18 20:27:40 +0100

    init: Interpolated colors
    Use baricentric coordinates to interpolate triangle vertex colors.

--- src/math/color.rs
+++ src/math/color.rs
@@ -2,6 +2,8 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+use std::ops::{Add, Mul};
+
 #[repr(C)]
 #[derive(Clone, Copy, Default, Debug, Eq, PartialEq)]
 /// This is the layout expected by the PNG class
@@ -25,6 +27,109 @@ impl From<u32> for RGBA8 {
             (color >> 16) as u8,
             (color >> 8) as u8,
             color as u8,
+        )
+    }
+}
+
+impl Add for RGBA8 {
+    type Output = RGBA8;
+
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self.r += rhs.r;
+        self.g += rhs.g;
+        self.b += rhs.b;
+        self.a += rhs.a;
+        self
+    }
+}
+
+impl Add for &RGBA8 {
+    type Output = RGBA8;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output::new(
+            self.r + rhs.r,
+            self.g + rhs.g,
+            self.b + rhs.b,
+            self.a + rhs.a,
+        )
+    }
+}
+
+impl Add<RGBA8> for &RGBA8 {
+    type Output = RGBA8;
+
+    fn add(self, rhs: RGBA8) -> Self::Output {
+        Self::Output::new(
+            self.r + rhs.r,
+            self.g + rhs.g,
+            self.b + rhs.b,
+            self.a + rhs.a,
+        )
+    }
+}
+
+impl Add<&RGBA8> for RGBA8 {
+    type Output = RGBA8;
+
+    fn add(self, rhs: &RGBA8) -> Self::Output {
+        Self::Output::new(
+            self.r + rhs.r,
+            self.g + rhs.g,
+            self.b + rhs.b,
+            self.a + rhs.a,
+        )
+    }
+}
+
+impl Mul<f32> for RGBA8 {
+    type Output = RGBA8;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::Output::new(
+            ((self.r as f32) * rhs) as u8,
+            ((self.g as f32) * rhs) as u8,
+            ((self.b as f32) * rhs) as u8,
+            ((self.a as f32) * rhs) as u8,
+        )
+    }
+}
+
+impl Mul<f32> for &RGBA8 {
+    type Output = RGBA8;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::Output::new(
+            ((self.r as f32) * rhs) as u8,
+            ((self.g as f32) * rhs) as u8,
+            ((self.b as f32) * rhs) as u8,
+            ((self.a as f32) * rhs) as u8,
+        )
+    }
+}
+
+impl Mul<&RGBA8> for f32 {
+    type Output = RGBA8;
+
+    fn mul(self, rhs: &RGBA8) -> Self::Output {
+        Self::Output::new(
+            (self * rhs.r as f32) as u8,
+            (self * rhs.g as f32) as u8,
+            (self * rhs.b as f32) as u8,
+            (self * rhs.a as f32) as u8,
+        )
+    }
+}
+
+impl Mul<&RGBA8> for &f32 {
+    type Output = RGBA8;
+
+    fn mul(self, rhs: &RGBA8) -> Self::Output {
+        Self::Output::new(
+            (self * rhs.r as f32) as u8,
+            (self * rhs.g as f32) as u8,
+            (self * rhs.b as f32) as u8,
+            (self * rhs.a as f32) as u8,
         )
     }
 }
--- src/triangle.rs
+++ src/triangle.rs
@@ -5,7 +5,7 @@
 use super::*;
 
 pub struct Triangle {
-    vertices: [Vertex; 3],
+    pub vertices: [Vertex; 3],
 }
 
 impl Triangle {
@@ -38,6 +38,7 @@ impl Intersect for Triangle {
         let v0v2 = v2 - v0;
         // No need to normalize
         let n = v0v1.cross(&v0v2);
+        let denom = n.dot(&n);
 
         // Step 1: finding P
 
@@ -76,7 +77,8 @@ impl Intersect for Triangle {
         let edge1 = v2 - v1;
         let vp1 = p - v1;
         let c = edge1.cross(&vp1);
-        if n.dot(&c) < 0.0 {
+        let u = n.dot(&c);
+        if u < 0.0 {
             return None; // P is on the right side
         }
 
@@ -84,13 +86,23 @@ impl Intersect for Triangle {
         let edge2 = v0 - v2;
         let vp2 = p - v2;
         let c = edge2.cross(&vp2);
-        if n.dot(&c) < 0.0 {
+        let v = n.dot(&c);
+        if v < 0.0 {
             return None; // P is on the right side;
         }
 
-        let point = ray.origin + ray.dir * t;
-        let hit = Hit::new(point);
+        let uv = Vec2::new(u / denom, v / denom);
+        let hit = Hit::new(p, uv);
         Some(hit) // This ray hits the triangle
+    }
+
+    fn get_color(&self, hit: &Hit) -> RGBA8 {
+        // Interpolate vertex colors
+        let c0 = &self.vertices[0].color;
+        let c1 = &self.vertices[1].color;
+        let c2 = &self.vertices[2].color;
+
+        (1.0 - hit.uv.x - hit.uv.y) * c2 + hit.uv.x * c0 + hit.uv.y * c1
     }
 
     fn get_normal(&self, _hit: &Hit) -> Vec3 {
