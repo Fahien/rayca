dbd066e1705eadbe64629df423e05eb9866859ae
Antonio Caggiano <info@antoniocaggiano.eu>
2022-03-19 17:40:35 +0100

    gltf: Load a model from file
    Reuse packs and model builder from rustspot to load a glTF model from
    file. Then implement a method to convert a primitive into a list of
    triangles, useful for the raytracing algorithm.

--- src/triangle.rs
+++ src/triangle.rs
@@ -4,19 +4,19 @@
 
 use super::*;
 
-pub struct Triangle {
-    pub vertices: [Vertex; 3],
+pub struct Triangle<V> {
+    pub vertices: [V; 3],
 }
 
-impl Triangle {
-    pub fn new(a: Vertex, b: Vertex, c: Vertex) -> Self {
+impl<V> Triangle<V> {
+    pub fn new(a: V, b: V, c: V) -> Self {
         Self {
             vertices: [a, b, c],
         }
     }
 }
 
-impl Default for Triangle {
+impl Default for Triangle<Vertex> {
     fn default() -> Self {
         Self::new(
             Vertex::new(-1.0, 0.0, 0.0),
@@ -26,12 +26,96 @@ impl Default for Triangle {
     }
 }
 
-impl Intersect for Triangle {
+impl Intersect for Triangle<Vertex> {
     /// [Ray-triangle intersection](https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/ray-triangle-intersection-geometric-solution)
     fn intersects(&self, ray: &Ray) -> Option<Hit> {
-        let v0 = &self.vertices[0].point;
-        let v1 = &self.vertices[1].point;
-        let v2 = &self.vertices[2].point;
+        let v0 = &self.vertices[0].pos;
+        let v1 = &self.vertices[1].pos;
+        let v2 = &self.vertices[2].pos;
+
+        // Plane's normal
+        let v0v1 = v1 - v0;
+        let v0v2 = v2 - v0;
+        // No need to normalize
+        let n = v0v1.cross(&v0v2);
+        let denom = n.dot(&n);
+
+        // Step 1: finding P
+
+        // Check if ray and plane are parallel
+        let n_dot_ray_dir = n.dot(&ray.dir);
+        if n_dot_ray_dir.abs() < f32::EPSILON {
+            // Parallel do not intersect
+            return None;
+        }
+        // Compute d parameter using equation 2
+        let d = -n.dot(&v0);
+
+        // Compute t (equation 3)
+        let t = -(n.dot(&ray.origin) + d) / n_dot_ray_dir;
+
+        // Check if the triangle is behind the ray
+        if t < 0.0 {
+            return None;
+        }
+
+        // Compute the intersection point using equation 1
+        let p = ray.origin + ray.dir * t;
+
+        // Step 2: inside-outside test
+
+        // Edge 0
+        let edge0 = v1 - v0;
+        let vp0 = p - v0;
+        // Vector perpendicular to triangle's plane
+        let c = edge0.cross(&vp0);
+        if n.dot(&c) < 0.0 {
+            return None; // P is on the right side
+        }
+
+        // Edge 1
+        let edge1 = v2 - v1;
+        let vp1 = p - v1;
+        let c = edge1.cross(&vp1);
+        let u = n.dot(&c);
+        if u < 0.0 {
+            return None; // P is on the right side
+        }
+
+        // Edge 2
+        let edge2 = v0 - v2;
+        let vp2 = p - v2;
+        let c = edge2.cross(&vp2);
+        let v = n.dot(&c);
+        if v < 0.0 {
+            return None; // P is on the right side;
+        }
+
+        let uv = Vec2::new(u / denom, v / denom);
+        let hit = Hit::new(p, uv);
+        Some(hit) // This ray hits the triangle
+    }
+
+    fn get_color(&self, hit: &Hit) -> Color {
+        // Interpolate vertex colors
+        let c0 = &self.vertices[0].color;
+        let c1 = &self.vertices[1].color;
+        let c2 = &self.vertices[2].color;
+
+        (1.0 - hit.uv.x - hit.uv.y) * c2 + hit.uv.x * c0 + hit.uv.y * c1
+    }
+
+    fn get_normal(&self, _hit: &Hit) -> Vec3 {
+        Vec3::new(0.0, 0.0, 1.0)
+    }
+}
+
+impl Intersect for Triangle<&Vertex> {
+    /// [Ray-triangle intersection](https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/ray-triangle-intersection-geometric-solution)
+    fn intersects(&self, ray: &Ray) -> Option<Hit> {
+        let v0 = &self.vertices[0].pos;
+        let v1 = &self.vertices[1].pos;
+        let v2 = &self.vertices[2].pos;
 
         // Plane's normal
         let v0v1 = v1 - v0;
@@ -116,7 +200,7 @@ mod test {
 
     #[test]
     fn intersect() {
-        let triangle = Triangle::default();
+        let triangle = Triangle::<Vertex>::default();
         let ray = Ray::new(Vec3::new(0.0, 0.0, 1.0), Vec3::new(0.0, 0.0, -1.0));
         assert!(triangle.intersects(&ray).is_some());
         let ray = Ray::new(Vec3::new(0.0, 0.0, 1.0), Vec3::new(0.0, 0.0, 1.0));
