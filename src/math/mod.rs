0b2c0827aeb25b1f5dcdfb3e8d2de69269a8cb86
Antonio Caggiano <info@antoniocaggiano.eu>
2022-03-20 15:59:12 +0100

    math: Mat4, Quat, and Trs
    Implement Matrix 4x4, Quaternion, and Translate-Rotation-Scale
    structures, with some math operations and conversions.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,10 +2,16 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
-pub mod vec3;
-pub mod ray;
 pub mod color;
+pub mod mat4;
+pub mod quat;
+pub mod ray;
+pub mod trs;
+pub mod vec3;
 
-pub use vec3::*;
-pub use ray::*;
 pub use color::*;
+pub use mat4::*;
+pub use quat::*;
+pub use ray::*;
+pub use trs::*;
+pub use vec3::*;
