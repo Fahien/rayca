fe2f46ad1520fcb4807f958d0e1b0747fe23666f
Antonio Caggiano <info@antoniocaggiano.eu>
2022-03-23 01:06:08 +0100

    draw: Collect triangles upfront
    Simple optimization: instead of creating triangles for primitives each
    time a pixel is processed, collect them once at the beginning.

--- src/scene.rs
+++ src/scene.rs
@@ -34,7 +34,7 @@ impl Scene {
         Ok(())
     }
 
-    fn draw_pixel(&self, ray: Ray, pixel: &mut RGBA8) {
+    fn draw_pixel(&self, ray: Ray, triangles: &[Triangle<Vertex>], pixel: &mut RGBA8) {
         let mut depth = f32::INFINITY;
 
         for obj in &self.objects {
@@ -47,23 +47,14 @@ impl Scene {
             }
         }
 
-        for model in &self.models {
-            for node in model.nodes.iter() {
-                if let Some(mesh) = model.meshes.get(node.mesh) {
-                    for prim_handle in mesh.primitives.iter() {
-                        let prim = model.primitives.get(*prim_handle).unwrap();
-                        for triangle in prim.triangles(Mat4::from(&node.trs)) {
-                            if let Some(hit) = triangle.intersects(&ray) {
-                                if hit.depth < depth {
-                                    depth = hit.depth;
-                                    //let color = triangle.get_color(&hit);
-                                    let n = triangle.get_normal(&hit);
-                                    let color = RGBA8::from(n);
-                                    *pixel = color;
-                                }
-                            }
-                        }
-                    }
+        for triangle in triangles {
+            if let Some(hit) = triangle.intersects(&ray) {
+                if hit.depth < depth {
+                    depth = hit.depth;
+                    //let color = triangle.get_color(&hit);
+                    let n = triangle.get_normal(&hit);
+                    let color = RGBA8::from(n);
+                    *pixel = color;
                 }
             }
         }
@@ -72,6 +63,21 @@ impl Scene {
 
 impl Draw for Scene {
     fn draw(&self, image: &mut Image) {
+        let mut triangles = vec![];
+
+        for model in &self.models {
+            for node in model.nodes.iter() {
+                if let Some(mesh) = model.meshes.get(node.mesh) {
+                    for prim_handle in mesh.primitives.iter() {
+                        let prim = model.primitives.get(*prim_handle).unwrap();
+                        let mut prim_triangles = prim.triangles(Mat4::from(&node.trs));
+                        triangles.append(&mut prim_triangles);
+                    }
+                }
+            }
+        }
+        println!("Collected {} triangles", triangles.len());
+
         let width = image.width() as f32;
         let height = image.height() as f32;
 
@@ -93,10 +99,10 @@ impl Draw for Scene {
                     let yy = (1.0 - 2.0 * ((y as f32 + 0.5) * inv_height)) * angle;
                     let mut dir = Vec3::new(xx, yy, -1.0);
                     dir.normalize();
-                    let origin = Vec3::new(0.0, 0.0, 4.0);
+                    let origin = Vec3::new(0.0, 0.0, 4.5);
                     let ray = Ray::new(origin, dir);
 
-                    self.draw_pixel(ray, row[x]);
+                    self.draw_pixel(ray, &triangles, row[x]);
                 }
             });
     }
