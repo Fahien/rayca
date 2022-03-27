b43e23029427c6c1bfecf6270ac6e1b8715abf95
Antonio Caggiano <info@antoniocaggiano.eu>
2022-03-27 17:55:26 +0200

    img: Albedo texture
    Load texture coordinates for each vertex and interpolate when sampling.

--- src/material.rs
+++ src/material.rs
@@ -6,12 +6,14 @@ use super::*;
 
 pub struct Material {
     pub color: Color,
+    pub albedo: Option<Handle<Texture>>,
 }
 
 impl Material {
     pub fn new() -> Self {
         Self {
             color: Color::white(),
+            albedo: None,
         }
     }
 }
--- src/math/vec2.rs
+++ src/math/vec2.rs
@@ -2,6 +2,8 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+use std::ops::{Add, Mul};
+
 #[repr(C)]
 #[derive(Clone, Copy, Default, Debug, PartialEq)]
 pub struct Vec2 {
@@ -12,5 +14,31 @@ pub struct Vec2 {
 impl Vec2 {
     pub fn new(x: f32, y: f32) -> Vec2 {
         Vec2 { x, y }
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::Output::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl Mul<f32> for &Vec2 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::Output::new(self.x * rhs, self.y * rhs)
     }
 }
