3c7b37f7ee7c59aa917d49d814a4b3995ec1c46b
Antonio Caggiano <info@antoniocaggiano.eu>
2022-04-11 16:55:09 +0200

    img: Color types
    Improve color support by correctly handling both RGB8 and RGBA8 images.

--- src/math/color/mod.rs
+++ src/math/color/mod.rs
@@ -2,9 +2,40 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+pub mod rgb8;
+pub mod rgba8;
+
+pub use rgb8::*;
+pub use rgba8::*;
+
 use crate::Vec3;
 
 use std::ops::{Add, Mul};
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum ColorType {
+    RGB8,
+    RGBA8,
+}
+
+impl ColorType {
+    pub fn channels(&self) -> usize {
+        match self {
+            ColorType::RGB8 => 3,
+            ColorType::RGBA8 => 4,
+        }
+    }
+}
+
+impl Default for ColorType {
+    fn default() -> Self {
+        Self::RGBA8
+    }
+}
+
+pub trait ColorTyped: Copy {
+    fn color_type() -> ColorType;
+}
 
 #[derive(Clone, Copy, Default, Debug, PartialEq)]
 pub struct Color {
@@ -171,54 +202,5 @@ impl Mul<Color> for Color {
             self.b * rhs.b,
             self.a * rhs.a,
         )
-    }
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Default, Debug, Eq, PartialEq)]
-/// This is the layout expected by the PNG class
-pub struct RGBA8 {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-    pub a: u8,
-}
-
-impl RGBA8 {
-    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
-        Self { r, g, b, a }
-    }
-}
-
-impl From<u32> for RGBA8 {
-    fn from(color: u32) -> Self {
-        Self::new(
-            (color >> 24) as u8,
-            (color >> 16) as u8,
-            (color >> 8) as u8,
-            color as u8,
-        )
-    }
-}
-
-impl From<Color> for RGBA8 {
-    fn from(color: Color) -> Self {
-        Self::new(
-            (color.r * 255.0) as u8,
-            (color.g * 255.0) as u8,
-            (color.b * 255.0) as u8,
-            (color.a * 255.0) as u8,
-        )
-    }
-}
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn construct() {
-        let black8 = RGBA8::new(0, 0, 0, 255);
-        let black32 = RGBA8::from(0x000000FF);
-        assert!(black8 == black32);
     }
 }
