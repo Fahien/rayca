059bd8f9fc0e1e3080a30495992c9b84c04d1e5a
Fahien <Fahien@users.noreply.github.com>
2022-12-04 18:44:47 +0000

    deploy: 24919e4aa9a066b1d8548b7349f428535a9aa091

--- bootstrap.js
+++ bootstrap.js
@@ -303,7 +303,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rayca_bg.wasm":"0e96d236ee764de0c062"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rayca_bg.wasm":"deb6d1ae1c89dae27501"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
