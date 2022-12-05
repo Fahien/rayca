8987aaa5e79dcac6b568b7d962339d63bdce4a3f
Antonio Caggiano <info@antoniocaggiano.eu>
2022-12-05 12:04:19 +0100

    img: Color types
    Improve color support by correctly handling both RGB8 and RGBA8 images.

--- src/log.rs
+++ src/log.rs
@@ -2,10 +2,19 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+pub use owo_colors::OwoColorize;
+
 #[cfg(not(target_arch = "wasm32"))]
 #[macro_export]
 macro_rules! rlog {
     ( $( $t:tt )* ) => {
         println!($( $t )*)
+    }
+}
+
+#[macro_export]
+macro_rules! fail {
+    ( $( $t:tt )* ) => {
+        format!("{:>12} {}", "Failed".red().bold(), $( $t )*)
     }
 }
